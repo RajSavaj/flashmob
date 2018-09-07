aecb025b3fd8bc6ea8315ff7285193f2e38e357b
Raj Savaj <rsavaj7@gmail.com>
2018-09-07 12:28:38 +0530

    make it better

--- functions/login/password.js
+++ functions/login/password.js
@@ -64,7 +64,7 @@ exports.resetPasswordInit = email =>
 			}
 		})
 
-		.then(user => {
+		.then(u => {
             var transporter = nodemailer.createTransport({
 			    host: 'smtp.gmail.com',
 			    port: 465,
